65374fe1f6611465bf002ea7abfadcef7131100c
shmuelblau <a0528075188@gmail.com>
2025-05-14 12:42:34 +0300

    conversion

--- ATBASH/Program.cs
+++ ATBASH/Program.cs
@@ -8,8 +8,17 @@ namespace ATBASH
 {
     internal class Program
     {
+        
+        public static string conversion(string input)
+        {
+            string output =string.Join("", input.Select(x => char.ToLower(x)).Select(s => (char)((int)s + ((103 - (int)s) * 2))).ToArray());
+            string[] a = output.Split(',');
+            return output;
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine(conversion("azaz"));
+            
         }
     }
 }
